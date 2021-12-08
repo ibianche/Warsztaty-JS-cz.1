4872242c11a4217de4880b21cf5608b7426d710c
ibianche <inna.pomorska@gmail.com>
2021-12-08 18:56:42 +0100

    function render TASK nr1

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,34 @@
-
 const select = {
-  templateOf:{
+  templateOf: {
     book: '#template-book',
-  }
+  },
+  containerOf: {
+    list: '.books-list',
+  },
 };
 
 const templates = {
   book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
 };
 
+
+function render() {               /*wzorowałam się metodą Product.renderInMenu*/
+
+  for (let book of dataSource.books) {
+    const bookInfo = {
+      id: book.id,
+      name: book.name,
+      price: book.price,
+      rating: book.rating,
+      image: book.image,
+    };
+
+    const generatedHTML = templates.book(bookInfo);
+    const newElement = utils.createDOMFromHTML(generatedHTML);
+    const listContainer = document.querySelector(select.containerOf.list);
+    listContainer.appendChild(newElement);
+
+  }
+}
+
+render();
