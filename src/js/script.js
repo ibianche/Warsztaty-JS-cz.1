ea7f0221551e9ef616e841a83cb2f1cb11ee90bf
ibianche <inna.pomorska@gmail.com>
2021-12-08 19:45:16 +0100

    initActions

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,19 @@ const select = {
   },
   containerOf: {
     list: '.books-list',
+    image: '.book___image'
   },
 };
 
 const templates = {
   book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
 };
+
+
+
+render();
+initActions();
+
 
 
 function render() {               /*wzorowałam się metodą Product.renderInMenu*/
@@ -27,8 +34,54 @@ function render() {               /*wzorowałam się metodą Product.renderInMen
     const newElement = utils.createDOMFromHTML(generatedHTML);
     const listContainer = document.querySelector(select.containerOf.list);
     listContainer.appendChild(newElement);
-
   }
 }
 
-render();
+
+
+const favoriteBooks =[];
+
+function initActions() {
+
+  const elementOfList = document.querySelector(select.containerOf.list);
+
+  elementOfList.addEventListener('dblclick', function (event) {
+    event.preventDefault();
+
+    elementOfList.classList.add('favorite');
+
+    const dataId = document.querySelector(dataSource.books.id);
+
+    const favoriteBooks = dataId;
+
+
+  });
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
