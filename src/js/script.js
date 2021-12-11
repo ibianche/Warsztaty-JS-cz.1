07918b21f932639f4d7745f522f3221c7ce4f8b8
ibianche <inna.pomorska@gmail.com>
2021-12-11 12:05:36 +0100

    function determineRatingBgc(rating) TASK nr 6

--- src/js/script.js
+++ src/js/script.js
@@ -119,16 +119,15 @@ function filterBooks() {
 
 
 function determineRatingBgc(rating) {
-  let background = [];
   if (rating < 6){
-    let background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
   }else if (rating > 6 && rating <= 8){
-    let background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
   }else if (rating  > 8 && rating <= 9){
-    let background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    return'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
   }else if(rating > 9){
-    let background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
-  } return background;
+    return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+  }
 
 
 
